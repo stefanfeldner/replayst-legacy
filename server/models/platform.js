683c4589b3d78dd02b1ecd9d9ac2cee21c4f8ebb
Filippo Ceron <kaim.aragonar@gmail.com>
2022-04-21 18:57:49 +0200

    changed database structure to work with multiple schemas

--- server/models/platform.js
+++ server/models/platform.js
@@ -1,27 +1,27 @@
-// const mongoose = require('./');
+const mongoose = require('./');
 
-// const { Schema } = mongoose;
+const { Schema } = mongoose;
 
-// const newPlatform = Schema({
-//   id: Number,
-//   slug: String,
-//   name: String,
-//   owned: {
-//     type: Boolean,
-//     default: false
-//   }
-// });
-
-// const Platform = mongoose.model('platform', newPlatform);
-
-// module.exports = Platform;
-
-module.exports = {
+const newPlatform = Schema({
   id: Number,
   slug: String,
   name: String,
   owned: {
     type: Boolean,
-    default: false,
-  },
-};
+    default: false
+  }
+});
+
+const Platform = mongoose.model('platform', newPlatform);
+
+module.exports = Platform;
+
+// module.exports = {
+//   id: Number,
+//   slug: String,
+//   name: String,
+//   owned: {
+//     type: Boolean,
+//     default: false,
+//   },
+// };
