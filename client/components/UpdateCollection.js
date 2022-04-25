309cab82ac509e2ccb1573bb21db193d8864e74f
Filippo Ceron <kaim.aragonar@gmail.com>
2022-04-25 11:55:03 +0200

    add icons for adding and removing from collection

--- client/components/UpdateCollection.js
+++ client/components/UpdateCollection.js
@@ -2,6 +2,7 @@ import { useContext, useState } from 'react';
 import { Pressable, StyleSheet, Text, View } from 'react-native';
 import { addGameToCollection } from '../services/DbClient';
 import { UserContext } from './UserContext';
+import { Ionicons } from '@expo/vector-icons';
 
 // TODO handle the remove from collection case, with an alert for accidental press
 export default function UpdateCollection({
@@ -13,7 +14,7 @@ export default function UpdateCollection({
   const { owned } = useContext(UserContext);
   const [tiles, setOwnedTiles] = owned;
 
-  const userId = '6266679c68159251ea6f845d'; //'6261e0b712592ddafe9b6aa2'; // TODO make it dynamic by user
+  const userId = '6266679c68159251ea6f845d'; // filo: '6261e0b712592ddafe9b6aa2'; // TODO make it dynamic by user
   return (
     <View style={styles.container}>
       <Pressable
@@ -37,7 +38,15 @@ export default function UpdateCollection({
               : null // TODO logic for game deletion
         }
       >
-        <Text style={styles.text}>{isAdded ? '-' : '+'}</Text>
+        {isAdded ? (
+          <Ionicons
+            name="ios-checkmark-circle-outline"
+            size={28}
+            color="#e9e7e3"
+          />
+        ) : (
+          <Ionicons name="ios-add-circle-outline" size={28} color="#c8c6bf" />
+        )}
       </Pressable>
     </View>
   );
@@ -48,8 +57,7 @@ const styles = StyleSheet.create({
     flex: 1,
     justifyContent: 'center',
     alignSelf: 'flex-end',
-    padding: 10,
-    width: '10%'
+    padding: 10
   },
   text: {
     color: 'rgb(222, 219, 214)',
